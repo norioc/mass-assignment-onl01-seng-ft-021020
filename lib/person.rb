f3a966117ba4f4c3b73ca625641512343a0b9b0d
norio c <norionorioc@gmail.com>
2020-02-27 04:51:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,12 @@ class Person
   
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, pant_length, :pant_width 
   
-  def initialize(attribute=nil)
+  def initialize(attributes)
+      attributes.each do |key, value|
+        self.send("#{key}=", value)
+      end
+  end
+end
       
   
   
